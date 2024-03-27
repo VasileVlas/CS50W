685b7cdc9f5f2dea1b9c7be8218dc08dd0baa293
VasileVlas <vasile.vlas@seznam.cz>
2024-03-27 22:04:54 +0100

    Much more things added

--- lecture3/tasks/urls.py
+++ lecture3/tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = "tasks" #identifier for urls when used in template html file as link
 urlpatterns = [
     path("", views.index, name="index"),
     path("add", views.add, name="add")
